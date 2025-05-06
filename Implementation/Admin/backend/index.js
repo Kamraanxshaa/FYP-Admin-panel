7d6a409ad1407fe6bf779ab2ab92e2fff3cd3917
Ismail0991 <ismailmohammad00001@gmail.com>
2025-05-07 01:20:46 +0500

    Restaurant API's Index

---
+++ Implementation/Admin/backend/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    getRestaurants: require('./getRestaurants'),
+    searchRestaurantsByName: require('./searchRestaurantsByName'),
+    deleteRestaurant: require('./deleteRestaurant'),
+    updateRestaurant: require('./updateRestaurant'),
+    addRestaurant: require('./addRestaurant'),
+    getUserByRestaurantId: require('./getUserByRestaurantId'),
+    
+
+  };
+  
